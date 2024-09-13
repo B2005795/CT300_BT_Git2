c3eab9aaf377d052b50b41fc5e87fca31ad7a8f7
B2005795 <hanb2005795@student.ctu.edu.vn>
2024-09-13 15:53:03 +0700

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String []args) {
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
         System.out.println("Hello World");
     }
 }
